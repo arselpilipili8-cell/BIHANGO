32854017781ba80ee991a91284b1ac6174910e5f
arselpilipili8-cell <arselpilipili8@gmail.com>
2026-07-16 23:46:36 +0100

    Refactor lireLDR function for improved output and error handling
    Updated the LDR reading function to use textContent for output and added error logging.

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-const ip = "http://192.168.79.163/ldr"; // Remplacer par l'adresse IP de l'ESP32
-
-async function lireLDR(){
-
-    try{
-
-        const reponse = await fetch(ip);
-
+async function lireLDR() {
+    try {
+        const reponse = await fetch("http://192.168.79.163/ldr");
         const valeur = await reponse.text();
 
-        document.getElementById("value").innerHTML = valeur;
-
-        let pourcentage=(valeur/4095)*100;
-
-        document.getElementById("bar").style.width=pourcentage+"%";
-
-        if(valeur<1200){
-
-            document.getElementById("etat").innerHTML="🌞 Forte lumière";
-
-        }
-
-        else if(valeur<2800){
-
-            document.getElementById("etat").innerHTML="🌤 Luminosité moyenne";
-
-        }
-
-        else{
-
-            document.getElementById("etat").innerHTML="🌙 Obscurité";
-
-        }
-
+        document.getElementById("ldr").textContent = valeur;
+    } catch (erreur) {
+        document.getElementById("ldr").textContent = "ESP32 déconnecté";
+        console.log(erreur);
     }
-
-    catch{
-
-        document.getElementById("etat").innerHTML="ESP32 déconnecté";
-
-    }
-
 }
 
-setInterval(lireLDR,1000);
+setInterval(lireLDR, 1000);
+
+lireLDR();
